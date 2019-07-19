a9d5a97fb1463d686d40c30bd8590bd139010cf0
KerrWu <wz_0818@163.com>
2019-07-19 20:28:07 +0800

    first commit

--- lib/networks.py
+++ lib/networks.py
@@ -6,6 +6,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.parallel
+from torchvision import models
 
 ##
 def weights_init(mod):
@@ -32,39 +33,42 @@ class Encoder(nn.Module):
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
-        main = nn.Sequential()
-        # input is nc x isize x isize
-        main.add_module('initial-conv-{0}-{1}'.format(nc, ndf),
-                        nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
-        main.add_module('initial-relu-{0}'.format(ndf),
-                        nn.LeakyReLU(0.2, inplace=True))
-        csize, cndf = isize / 2, ndf
+        main = models.resnet50(pretrained=True)
+        main = main.layer4
 
-        # Extra layers
-        for t in range(n_extra_layers):
-            main.add_module('extra-layers-{0}-{1}-conv'.format(t, cndf),
-                            nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
-            main.add_module('extra-layers-{0}-{1}-batchnorm'.format(t, cndf),
-                            nn.BatchNorm2d(cndf))
-            main.add_module('extra-layers-{0}-{1}-relu'.format(t, cndf),
-                            nn.LeakyReLU(0.2, inplace=True))
-
-        while csize > 4:
-            in_feat = cndf
-            out_feat = cndf * 2
-            main.add_module('pyramid-{0}-{1}-conv'.format(in_feat, out_feat),
-                            nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
-            main.add_module('pyramid-{0}-batchnorm'.format(out_feat),
-                            nn.BatchNorm2d(out_feat))
-            main.add_module('pyramid-{0}-relu'.format(out_feat),
-                            nn.LeakyReLU(0.2, inplace=True))
-            cndf = cndf * 2
-            csize = csize / 2
-
-        # state size. K x 4 x 4
-        if add_final_conv:
-            main.add_module('final-{0}-{1}-conv'.format(cndf, 1),
-                            nn.Conv2d(cndf, nz, 4, 1, 0, bias=False))
+        # main = nn.Sequential()
+        # # input is nc x isize x isize
+        # main.add_module('initial-conv-{0}-{1}'.format(nc, ndf),
+        #                 nn.Conv2d(nc, ndf, 4, 2, 1, bias=False))
+        # main.add_module('initial-relu-{0}'.format(ndf),
+        #                 nn.LeakyReLU(0.2, inplace=True))
+        # csize, cndf = isize / 2, ndf
+        #
+        # # Extra layers
+        # for t in range(n_extra_layers):
+        #     main.add_module('extra-layers-{0}-{1}-conv'.format(t, cndf),
+        #                     nn.Conv2d(cndf, cndf, 3, 1, 1, bias=False))
+        #     main.add_module('extra-layers-{0}-{1}-batchnorm'.format(t, cndf),
+        #                     nn.BatchNorm2d(cndf))
+        #     main.add_module('extra-layers-{0}-{1}-relu'.format(t, cndf),
+        #                     nn.LeakyReLU(0.2, inplace=True))
+        #
+        # while csize > 4:
+        #     in_feat = cndf
+        #     out_feat = cndf * 2
+        #     main.add_module('pyramid-{0}-{1}-conv'.format(in_feat, out_feat),
+        #                     nn.Conv2d(in_feat, out_feat, 4, 2, 1, bias=False))
+        #     main.add_module('pyramid-{0}-batchnorm'.format(out_feat),
+        #                     nn.BatchNorm2d(out_feat))
+        #     main.add_module('pyramid-{0}-relu'.format(out_feat),
+        #                     nn.LeakyReLU(0.2, inplace=True))
+        #     cndf = cndf * 2
+        #     csize = csize / 2
+        #
+        # # state size. K x 4 x 4
+        # if add_final_conv:
+        #     main.add_module('final-{0}-{1}-conv'.format(cndf, 1),
+        #                     nn.Conv2d(cndf, nz, 4, 1, 0, bias=False))
 
         self.main = main
 
@@ -86,19 +90,28 @@ class Decoder(nn.Module):
         self.ngpu = ngpu
         assert isize % 16 == 0, "isize has to be a multiple of 16"
 
-        cngf, tisize = ngf // 2, 4
-        while tisize != isize:
-            cngf = cngf * 2
-            tisize = tisize * 2
-
         main = nn.Sequential()
-        # input is Z, going into a convolution
+
+        cngf, tisize = ngf // 2, 14
+
         main.add_module('initial-{0}-{1}-convt'.format(nz, cngf),
                         nn.ConvTranspose2d(nz, cngf, 4, 1, 0, bias=False))
         main.add_module('initial-{0}-batchnorm'.format(cngf),
                         nn.BatchNorm2d(cngf))
         main.add_module('initial-{0}-relu'.format(cngf),
                         nn.ReLU(True))
+
+        while tisize != isize:
+            cngf = cngf // 2
+            tisize = tisize * 2
+
+            # input is Z, going into a convolution
+            main.add_module('initial-{0}-{1}-convt'.format(cngf*2, cngf),
+                            nn.ConvTranspose2d(cngf*2, cngf, 4, 1, 0, bias=False))
+            main.add_module('initial-{0}-batchnorm'.format(cngf),
+                            nn.BatchNorm2d(cngf))
+            main.add_module('initial-{0}-relu'.format(cngf),
+                            nn.ReLU(True))
 
         csize, _ = 4, cngf
         while csize < isize // 2:
